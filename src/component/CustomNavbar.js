406f54861393ed353872a3599d8d56c95bdfdd08
dummyaalena <dummyaalina@gmail.com>
2018-10-18 09:22:09 +0545

    React project Initialization, Flexbox on navbar

--- src/component/CustomNavbar.js
+++ src/component/CustomNavbar.js
@@ -1,29 +1,53 @@
 import React from 'react';
-import {Navbar, Table, Dropdown} from 'react-bootstrap';
-class Navbar extends React.Component{
-    render(){
-        return(
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Navbar>
-        )
-    }
+import {Container, Col, Row} from 'react-bootstrap';
+import hamburgerimg from '../icn_menu.png';
+import '../css/customnavbar.css';
+import {ic_home} from 'react-icons-kit/md/ic_home';
+import {rocket} from 'react-icons-kit/entypo/rocket';
+import {thLargeOutline} from 'react-icons-kit/typicons/thLargeOutline';
+import {support} from 'react-icons-kit/typicons/support';
+import {Icon} from 'react-icons-kit';
+
+class CustomNavbar extends React.Component{
+  render() {
+    return (
+        <div className="nav">
+            <Container fluid={true}>
+                <Row className="h-100 align-items-center">
+                    <Col md={6} xs="auto" className="nav_brand d-flex flex-row">
+                        <div className="ham_icon" > </div>
+                        <div className="nav_menu">{' Menu '}</div>
+                        <div className="vertical_line"> </div>
+                        <div>
+                            <Icon className="home_icon" size={32} icon={ic_home}/>
+                            <span className="workspace_name"> {' insightworkshop'}
+                            </span>
+                        </div>
+                    </Col>
+                    <Col md={6} xs="auto">
+                        <div className="d-flex justify-content-end right_icons">
+                            <a href="#">
+                                <span className="icon"><Icon size={32} icon={rocket}/></span>
+                                <span className="label">Plans</span>
+                            </a>
+                            <a href="#">
+                                <span className="icon"><Icon size={32} icon={thLargeOutline}/></span>
+                                <span className="label">Workspaces</span>
+                            </a>
+                            <a href="#">
+                                <span className="icon"><Icon size={32} icon={support}/></span>
+                                <span className="label">Help</span>
+                            </a>
+                            <a href="#">
+                                <img className="icon icon--slack_img" src="https://a.slack-edge.com/66f9/img/icons/ios-64.png" title="Slack" alt="Launch Slack" />
+                                <span className="label">Launch</span>
+                            </a>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+  }
 }
+export default CustomNavbar;
